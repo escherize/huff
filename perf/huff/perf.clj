0c852e7f22037a25db073639ae10780be5c99849
Bryan Maass <bryan.maass@gmail.com>
2023-07-07 10:44:11 -0600

    adding perf info

--- perf/huff/perf.clj
+++ perf/huff/perf.clj
@@ -1,38 +1,52 @@
 (ns huff.perf
   (:require
+   [clojure.java.io :as io]
    [criterium.core :as bench]
+   [hiccup.core :as hiccup]
    [huff.core :as h]
    [taoensso.tufte :as tufte :refer (defnp p profiled profile)]))
 
 ;; `page.edn` is wikipedia's list of common misconceptions passed through html2hiccup.
 
 ;; we support fragments:
-(defonce page-edn (read-string (str "[:<> " (slurp "resources/page.edn") "]")))
+(defonce big-page (read-string (str "[:<> " (slurp (io/resource "big_page.edn")) "]")))
+(defonce medium-page (read-string (str "[:<> " (slurp (io/resource "medium_page.edn")) "]")))
+
 
 (defn avg [xs] (/ (reduce + xs) (count xs)))
 
+(defn run-big-hiccup [] (do (hiccup/html (drop 1 big-page)) :done))
+(defn run-medium-hiccup [] (do (hiccup/html (drop 1 medium-page)) :done))
+
+(println "# Benching with big-page")
+(println "### hiccup")
+(println "```")
+(bench/bench (hiccup/html (drop 1 big-page)))
+(println "```")
+(println "### pre-compiled hiccup template")
+(println "```")
+(bench/bench (run-big-hiccup))
+(println "```")
+(println "### huff")
+(println "```")
+(bench/bench (h/html big-page))
+(println "```")
+
+(println "# Benching with medium_page")
+(println "### hiccup")
+(println "```")
+(bench/bench (hiccup/html (drop 1 medium-page)))
+(println "```")
+(println "### pre-compiled hiccup template")
+(println "```")
+(bench/bench (run-medium-hiccup))
+(println "```")
+(println "### huff")
+(println "```")
+(bench/bench (h/html medium-page))
+(println "```")
+
+
 (comment
   (tufte/add-basic-println-handler! {})
-  (profile {} (h/html page-edn)))
-
-(comment
-
-  (require '[hiccup.core :as hiccup])
-
-  ;; drop 1 because no fragment support, but lists do work:
-  (time (def oo (hiccup/html (drop 1 page-edn))))
-
-  (time (def xx (h/html page-edn)))
-
-  (do
-    (println "### Benching hiccup\n```")
-    (bench/bench (hiccup/html (drop 1 page-edn)))
-    (println "```\n### Benching huff\n```")
-    (bench/bench (h/html page-edn))
-    (println "```"))
-
-  (try (h/html {:a :b})
-       (catch Exception e [(ex-message e) (ex-data e)]))
-;; => ["Invalid huff form passed to html. See `huff.core/hiccup-schema` for more info"
-;;     {:value {:a :b}}]
-  )
+  (profile {} (h/html big-page)))
