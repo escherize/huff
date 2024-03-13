1b1cff999a73e7c46aa6641b7b384d9e8410586a
Bryan Maass <bryan.maass@gmail.com>
2024-03-12 22:11:53 -0600

    - return raw strings so calls to h/html are composable - add px, etc convenience functions, and stop auto-pxing

--- test/huff/core2_test.clj
+++ test/huff/core2_test.clj
@@ -9,63 +9,67 @@
   (is (= (h/html [:div {:class nil}]) (h/html [:div])))
   (is (= (h/html [:div.a]) (h/html [:div {:class ["a" nil "" ""]}]))))
 
-(deftest simple-html-test
-  (is (= "<div class=\"x\">y</div>" (h/html [:div {:class "x"} "y"])))
+(defn as-string [f]
+  (let [sb (StringBuilder.)
+        append! (fn append! [& strings]
+                  (doseq [s strings
+                          :when s] (.append ^StringBuilder sb s)))]
+    (f append!)
+    (str sb)))
 
-  (is (= "<div class=\"x g\">y</div>" (h/html [:div.g {:class "x"} "y"])))
+(deftest simple-html-test
+  (is (= "<div class=\"x\">y</div>" (str (h/html [:div {:class "x"} "y"]))))
+
+  (is (= "<div class=\"x g\">y</div>" (str (h/html [:div.g {:class "x"} "y"]))))
 
   (is (=  "<div class=\"parent\"><div class=\"me-go-onto-child child\" id=\"me-too\">y</div></div>"
-          (h/html [:.parent>div.child {:class "me-go-onto-child" :id "me-too"} "y"])))
+          (str (h/html [:.parent>div.child {:class "me-go-onto-child" :id "me-too"} "y"]))))
 
-  (is (= "<div></div>" (h/html [:div])))
+  (is (= "<div></div>" (str (h/html [:div]))))
 
   (is (= "<div>x</div>"
-         (h/html [:div "x"])))
+         (str (h/html [:div "x"]))))
 
-  (is (= (h/html [:div {:style {:color "red" :background "black"}} "x" [:div {:style {:padding "10px"}} "z"] "y"])
-         "<div style=\"background:black;color:red;\">x<div style=\"padding:10px;\">z</div>y</div>"))
+  (is (str (h/html [:div {:style {:color "red" :background "black"}} "x" [:div {:style {:padding "10px"}} "z"] "y"])
+         (= "<div style=\"background:black;color:red;\">x<div style=\"padding:10px;\">z</div>y</div>")))
 
   (is (= "<div id=\"id\" class=\"class\"></div>"
-         (h/html [:div.class#id])))
+         (str (h/html [:div.class#id]))))
 
   (is (true?
-       (= (h/html [:div [:<> [:<> [:<> "x"]]]])
-          (h/html [:div [:<> [:<> "x"]]])
-          (h/html [:div [:<> "x"]]))))
+        (= (str (h/html [:div [:<> [:<> [:<> "x"]]]]))
+           (str (h/html [:div [:<> [:<> "x"]]]))
+           (str (h/html [:div [:<> "x"]])))))
 
-  (is (= (h/html [:h1 {:style {:border "1px solid red"}} "k"])
-         "<h1 style=\"border:1px solid red;\">k</h1>"))
+  (is (= "<h1 style=\"border:1px solid red;\">k</h1>"
+         (str (h/html [:h1 {:style {:border "1px solid red"}} "k"]))))
 
-  (is (= (h/html [:p "raw x"])
+  (is (= (str (h/html [:p "raw x"]))
          (str "<p>" (h/html {:allow-raw true}
                             [:hiccup/raw-html "raw x"]) "</p>")))
 
-  (is (= (h/html [:ul (for [n (range 3)] [:li n])])
-         "<ul><li>0</li><li>1</li><li>2</li></ul>"))
+  (is (= "<ul><li>0</li><li>1</li><li>2</li></ul>"
+         (str (h/html [:ul (for [n (range 3)] [:li n])]))))
 
-  (is (= (let [double (fn double [hic] [:<> hic hic])
+  (is (= "<p><div><p>hello.</p><ul><li>3</li><li>4</li></ul><pre>pre says: 1|2</pre></div><div><p>hello.</p><ul><li>3</li><li>4</li></ul><pre>pre says: 1|2</pre></div></p>"
+         (let [double (fn double [hic] [:<> hic hic])
                pre (fn pre [& xs]
                      [:div
                       [:p "hello."]
                       [:<>
                        [:ul (for [x (range 3 5)] [:li x])]
                        [:pre "pre says: " (str/join "|" xs)]]])]
-           (h/html [:p [double [pre 1 2]]]))
-         "<p><div><p>hello.</p><ul><li>3</li><li>4</li></ul><pre>pre says: 1|2</pre></div><div><p>hello.</p><ul><li>3</li><li>4</li></ul><pre>pre says: 1|2</pre></div></p>")))
+           (str (h/html [:p [double [pre 1 2]]]))))))
 
 (deftest style-test
-  (is (= "<div style=\"border:1px solid red;color:red;\">x</div>"
-        (h/html [:div {:style {:color "red" :border "1px solid red"}} "x"])))
-  (is (= "<div style=\"border:1px solid red;\">x</div>"
-        (h/html [:div {:style {:border "1px solid red"}} "x"])))
-  (is (= "<div style=\"border: 1px solid red\">x</div>"
-        (h/html [:div {:style "border: 1px solid red"} "x"]))))
 
-(defn as-string [f]
-  (let [sb (StringBuilder.)
-        append! (fn append! [& strings] (doseq [s strings :when s] (.append ^StringBuilder sb s)))]
-    (f append!)
-    (str sb)))
+  (is (= "<div style=\"border:1px solid red;color:red;\">x</div>"
+         (str (h/html [:div {:style {:color "red" :border "1px solid red"}} "x"]))))
+  (is (= "<div style=\"border:1px solid red;\">x</div>"
+         (str (h/html [:div {:style {:border "1px solid red"}} "x"]))))
+  (is (= "<div style=\"border: 1px solid red\">x</div>"
+         (str (h/html [:div {:style "border: 1px solid red"} "x"])))))
+
 
 (deftest attr-emission-test
   (is (= " id=\"x\" class=\"x y\"" (as-string #(#'h/emit-attrs % {:id "x" :class ["x" "y"]}))))
@@ -77,49 +81,53 @@
   (is (= (h/page {:allow-raw true} [:h1 "hi"]) "<!doctype html><h1>hi</h1>")))
 
 (deftest escape-test
-  (is (= "<div>&amp;</div>"  (h/html [:div "&"])))
-  (is (= "<div>&lt;</div>"   (h/html [:div "<"])))
-  (is (= "<div>&gt;</div>"   (h/html [:div ">"])))
-  (is (= "<div>&quot;</div>" (h/html [:div "\""])))
-  (is (= "<div>&#39;</div>"  (h/html [:div "\\"])))
+  (is (= "<div>&amp;</div>"  (str (h/html [:div "&"]))))
+  (is (= "<div>&lt;</div>"   (str (h/html [:div "<"]))))
+  (is (= "<div>&gt;</div>"   (str (h/html [:div ">"]))))
+  (is (= "<div>&quot;</div>" (str (h/html [:div "\""]))))
+  (is (= "<div>&#39;</div>"  (str (h/html [:div "\\"]))))
   (is (= "<div>&amp;</div><div>&lt;</div><div>&gt;</div><div>&quot;</div><div>&#39;</div>"
-         (h/html [:<>
-                  [:div "&"]
-                  [:div "<"]
-                  [:div ">"]
-                  [:div "\""]
-                  [:div "\\"]]))))
+         (str (h/html [:<>
+                       [:div "&"]
+                       [:div "<"]
+                       [:div ">"]
+                       [:div "\""]
+                       [:div "\\"]])))))
 
 (deftest unescape-test
   (is (= "<div>\"</div>"
-         (binding [h/*escape?* false] (h/html [:div "\""])))))
+         (binding [h/*escape?* false] (str (h/html [:div "\""]))))))
 
 (deftest style-attr-args-test
   (is (= "<div style=\"background-color:red;\"></div>"
-         (h/html [:div {:style {:background-color "red"}}]))))
+         (str (h/html [:div {:style {:background-color "red"}}])))))
 
-
-(deftest style-attr-numeric-values-append-px
-  (is (= (h/html [:div {:style {:width (* 5 2)}}])
+(deftest style-attr-numeric-values-append-px-test
+  (is (= (str (h/html [:div {:style {:width (h/px (* 5 2))}}]))
          "<div style=\"width:10px;\"></div>"))
-  (is (= (h/html [:div {:style {:height (* 50 2)}}])
+  (is (= (str (h/html [:div {:style {:height (h/px (* 50 2))}}]))
          "<div style=\"height:100px;\"></div>")))
 
-(deftest dot-shortcut-for-div
-  (is (= (#'h/tag->tag+id+classes :.) (#'h/tag->tag+id+classes :div))))
+(deftest do-not-pxify-every-attr-test
+  (is (= "<h1 style=\"opacity:1;width:20px;\"></h1>"
+         (str (h/html [:h1 {:style {:width (-> 20 h/px) :opacity 1}}])))))
+
+(deftest dot-shortcut-for-div-test
+  (is (= (#'h/tag->tag+id+classes :.)
+         (#'h/tag->tag+id+classes :div))))
 
 (deftest lists-work-for-spreading
-  (is (= "<div><span>ok</span></div>" (h/html [:div '([:span "ok"])])))
-  (is (= "<div></div>" (h/html [:<> '([:div])]))))
+  (is (= "<div><span>ok</span></div>" (str (h/html [:div '([:span "ok"])]))))
+  (is (= "<div></div>" (str (h/html [:<> '([:div])])))))
 
 (deftest html-raw-disallowed-test
   (is (= {:content "<script>mine bitcoin</script>", :allow-raw false}
-         (try (h/html [:hiccup/raw-html "<script>mine bitcoin</script>"])
+         (try (str (h/html [:hiccup/raw-html "<script>mine bitcoin</script>"]))
               (catch Exception e (ex-data e)))))
 
   (is (= "<script>mine bitcoin</script>"
-         (h/html {:allow-raw true}
-                 [:hiccup/raw-html "<script>mine bitcoin</script>"]))))
+         (str (h/html {:allow-raw true}
+                      [:hiccup/raw-html "<script>mine bitcoin</script>"])))))
 
 (deftest page-raw-allowed-test
   (is (= {:content "hi", :allow-raw false}
@@ -133,19 +141,41 @@
   (is (= "<!doctype html><div>select * from students where name = 'Bob Robert'); DROP TABLE Students;--</div>"
          (h/page [:div
                   "select * from students where name = 'Bob "
-                  [:hiccup/raw-html (h/raw-string "Robert'); DROP TABLE Students;--")]]))))
+                  [:hiccup/raw-html (h/raw "Robert'); DROP TABLE Students;--")]]))))
 
 (deftest you-can-invalidate-your-own-html
   (is (= "<!doctype html><div>My Page<ul><li><!-- hehe...</li><li>what happened...</li></ul></div>"
          (h/page [:div "My Page"
                   [:ul
-                   [:li [:hiccup/raw-html (h/raw-string "<!-- hehe...")]]
+                   [:li [:hiccup/raw-html (h/raw "<!-- hehe...")]]
                    [:li "what happened..."]]]))))
 
-(deftest users-cannot-invalidate-your-own-html
+(deftest users-cannot-invalidate-your-own-html-test
   (is (= {:content "<!-- hehe...", :allow-raw false}
          (try (h/page [:div "My Page"
                        [:ul
                         [:li [:hiccup/raw-html "<!-- hehe..."]]
                         [:li "what happened..."]]])
               (catch Exception e (ex-data e))))))
+
+(deftest compiled-huff-is-a-raw-string-primative
+  (is (= "<div><div>hi</div></div>"
+         (str (h/html [:div (h/html [:div "hi"])])))))
+
+(deftest styles-with-convenience-functions-test
+  (is (= "<div style=\"width:10px;\"></div>"
+         (str (h/html [:div {:style {:width (-> 10 h/px)}}]))))
+  (is (= "<div style=\"width:10em;\"></div>"
+         (str (h/html [:div {:style {:width (-> 10 h/em)}}]))))
+  (is (= "<div style=\"width:10rem;\"></div>"
+         (str (h/html [:div {:style {:width (-> 10 h/rem)}}]))))
+  (is (= "<div style=\"width:10%;\"></div>"
+         (str (h/html [:div {:style {:width (-> 10 h/pct)}}]))))
+  (is (= "<div style=\"width:10vw;\"></div>"
+         (str (h/html [:div {:style {:width (-> 10 h/vw)}}]))))
+  (is (= "<div style=\"width:10vh;\"></div>"
+         (str (h/html [:div {:style {:width (-> 10 h/vh)}}]))))
+  (is (= "<div style=\"width:10vmin;\"></div>"
+         (str (h/html [:div {:style {:width (-> 10 h/vmin)}}]))))
+  (is (= "<div style=\"width:10vmax;\"></div>"
+         (str (h/html [:div {:style {:width (-> 10 h/vmax)}}])))))
